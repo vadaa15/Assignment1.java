32ffde911c5164ffcf1cc1ba5898c39eb91f01ec
Vadaanya Bansal <vadaanya.bansal_cs24@gla.ac.in>
2025-08-08 15:27:24 +0530

    Create Asssignment1.java

---
+++ Asssignment1.java
@@ -0,0 +1,69 @@
+//1. Create a class Student info with a method displayInfo()  that prints your name , age ,and grade.
+ //public class student_info {
+	 //public void displayinfo() {
+		 ///System.out.println("Name: Aman Sharma");
+			//System.out.println("Age: 16");
+			//System.out.println("Grade: 10th");
+		  //}
+	 //public static void main(String[]arg) {
+		 //student_info obj = new student_info();
+		 //obj.displayinfo();
+		 
+	// }
+//	}
+// 2.Create a class Calculator with a method addNumber() that add two numbers and print the result.  
+//public class Calculator {
+	//public void addnumber() {
+		//int a = 40;
+		//int b =60;
+		//int sum = a+b;
+		//System.out.println("sum=" +sum);
+	//}
+
+	//public static void main(String[] args) {
+		//Calculator obj=new Calculator();
+		//obj.addnumber();	
+		//}
+	//}
+//3.Create a class Greeting with a method sayHello() that takes your name as a parameter and prints:
+//public class Greeting {
+	//public void sayHello(String name ) {
+		//System.out.println("Hello,"+name +"!Welcame to java programming .");
+	//}
+
+	//public static void main(String[] args) {
+		//Greeting obj=new Greeting();
+		//obj.sayHello("Tanya Agrawal ");
+	//}
+//4.Create a class SimpleIntrest with a method calculateIntrest() that calculates simple interest .
+//}public class SimpleIntrest {
+
+	//	public void calculateIntrest() {
+		//	int Principle = 1000;
+			//int time = 2;
+			//int Rate = 5;
+			//int SI = (Principle*time*Rate)/100;
+			//System.out.println("SI=" + SI);
+//		}
+
+		//public static void main(String[] args) {
+			//SimpleIntrest obj=new SimpleIntrest ();
+			//obj.calculateIntrest();
+
+		//}
+
+	//}
+// 5. Create a class Circle with a method calculateArea() that calculates and print the area of circle .
+//public class Circle {
+	//public void calculateArea() {
+		
+		//int radius = 7;
+		//double pi = 3.14;
+		//double Area =  pi*radius*radius;
+		//System.out.println("Area= "+ Area);
+	//}
+	//public static void main(String[] args) {
+		//Circle obj=new Circle();
+		//obj.calculateArea();
+	//}
+//}
